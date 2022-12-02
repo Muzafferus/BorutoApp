ace45ba8bb91751a454398a2d86807b8a635ca18
muzeffer.pasazade <muzeffer.pasazade@1tech.co>
2022-12-02 09:53:31 +0400

    Create HeroRemoteKey dao and model

--- app/src/main/java/com/muzafferus/borutoapp/data/local/dao/BorutoDatabase.kt
+++ app/src/main/java/com/muzafferus/borutoapp/data/local/dao/BorutoDatabase.kt
@@ -2,10 +2,13 @@ package com.muzafferus.borutoapp.data.local.dao
 
 import androidx.room.Database
 import com.muzafferus.borutoapp.domain.model.Hero
+import com.muzafferus.borutoapp.domain.model.HeroRemoteKey
 
-@Database(entities = [Hero::class], version = 1)
+@Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
 abstract class BorutoDatabase {
 
     abstract fun heroDao(): HeroDao
+
+    abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
 
 }
--- app/src/main/java/com/muzafferus/borutoapp/util/Constants.kt
+++ app/src/main/java/com/muzafferus/borutoapp/util/Constants.kt
@@ -3,5 +3,7 @@ package com.muzafferus.borutoapp.util
 object Constants {
 
     const val DETAILS_ARGUMENT_KEY = "heroId"
+
     const val HERO_DATABASE_TABLE = "hero_table"
+    const val HERO_REMOTE_KEY_DATABASE_TABLE = "hero_remote_key_table"
 }
