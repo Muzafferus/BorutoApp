e10554bb15ec7cd7c6e931b3c605a5480f94188a
muzeffer.pasazade <muzeffer.pasazade@1tech.co>
2022-12-02 20:43:39 +0400

    Add TypeConverter for DataBase

--- app/src/main/java/com/muzafferus/borutoapp/data/local/BorutoDatabase.kt
+++ app/src/main/java/com/muzafferus/borutoapp/data/local/BorutoDatabase.kt
@@ -1,10 +1,14 @@
-package com.muzafferus.borutoapp.data.local.dao
+package com.muzafferus.borutoapp.data.local
 
 import androidx.room.Database
+import androidx.room.TypeConverters
+import com.muzafferus.borutoapp.data.local.dao.HeroDao
+import com.muzafferus.borutoapp.data.local.dao.HeroRemoteKeyDao
 import com.muzafferus.borutoapp.domain.model.Hero
 import com.muzafferus.borutoapp.domain.model.HeroRemoteKey
 
 @Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class BorutoDatabase {
 
     abstract fun heroDao(): HeroDao
