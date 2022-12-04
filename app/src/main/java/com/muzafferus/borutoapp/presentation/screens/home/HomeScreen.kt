3bb59a60dd3e12e79c5c95a50a46c258ef9c08fa
muzeffer.pasazade <muzeffer.pasazade@1tech.co>
2022-12-05 01:49:37 +0400

    Add Calculation for RatingWidget and Complete

--- app/src/main/java/com/muzafferus/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/muzafferus/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,10 +1,14 @@
 package com.muzafferus.borutoapp.presentation.screens.home
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.muzafferus.borutoapp.presentation.components.RatingWidget
+import com.muzafferus.borutoapp.ui.theme.LARGE_PADDING
 
 @Composable
 fun HomeScreen(
@@ -19,7 +23,7 @@ fun HomeScreen(
             //OnClickTopBarSearch - Navigate to SearchScreen
         })
     }) {
-
+        RatingWidget(modifier = Modifier.padding(all = LARGE_PADDING), rating = 4.1)
     }
 
 
