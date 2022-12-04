f3d5bf0cfe3a3b327477cd4318ed5480ae782de9
muzeffer.pasazade <muzeffer.pasazade@1tech.co>
2022-12-05 02:59:46 +0400

    Design HeroItem

--- app/src/main/java/com/muzafferus/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/muzafferus/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,30 +1,29 @@
 package com.muzafferus.borutoapp.presentation.screens.home
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
-import com.muzafferus.borutoapp.presentation.components.RatingWidget
-import com.muzafferus.borutoapp.ui.theme.LARGE_PADDING
+import com.muzafferus.borutoapp.presentation.common.ListContent
 
 @Composable
 fun HomeScreen(
     navController: NavHostController,
     homeViewModel: HomeViewModel = hiltViewModel()
 ) {
-
     val allHeroes = homeViewModel.getAllHeroes.collectAsLazyPagingItems()
 
-    Scaffold(topBar = {
-        HomeTopBar(onSearchClicked = {
-            //OnClickTopBarSearch - Navigate to SearchScreen
+    Scaffold(
+        topBar = {
+            HomeTopBar(onSearchClicked = {
+                //OnClickTopBarSearch - Navigate to SearchScreen
+            })
+        },
+        content = {
+            ListContent(
+                heroes = allHeroes,
+                navController = navController
+            )
         })
-    }) {
-        RatingWidget(modifier = Modifier.padding(all = LARGE_PADDING), rating = 2.6)
-    }
-
-
 }
--- app/src/main/java/com/muzafferus/borutoapp/util/Constants.kt
+++ app/src/main/java/com/muzafferus/borutoapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.muzafferus.borutoapp.util
 
 object Constants {
 
-    const val BASE_URL = "http://10.0.2.2:8080/"
+    const val BASE_URL = "http://10.0.2.2:8080"
 
     const val DETAILS_ARGUMENT_KEY = "heroId"
 
