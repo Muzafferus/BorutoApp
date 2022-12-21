c9883265b89669fa1f81d4b8c953fedc64fafafe
muzeffer.pasazade <muzeffer.pasazade@1tech.co>
2022-12-21 11:23:37 +0400

    Show Empty or first time Search screen

--- app/src/main/java/com/muzafferus/borutoapp/presentation/common/ListContent.kt
+++ app/src/main/java/com/muzafferus/borutoapp/presentation/common/ListContent.kt
@@ -80,6 +80,10 @@ fun handlePagingResult(
                 EmptyScreen(error = error)
                 false
             }
+            heroes.itemCount < 1 -> {
+                EmptyScreen()
+                false
+            }
             else -> true
         }
     }
