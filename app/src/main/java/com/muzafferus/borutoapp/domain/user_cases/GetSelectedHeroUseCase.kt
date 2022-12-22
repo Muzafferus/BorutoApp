c8373eba0923afc9f7652f3ed21e13d751e4acb5
muzeffer.pasazade <muzeffer.pasazade@1tech.co>
2022-12-22 09:49:18 +0400

    Create DetailsViewModel and get selected hero info

--- app/src/main/java/com/muzafferus/borutoapp/domain/user_cases/GetSelectedHeroUseCase.kt
+++ app/src/main/java/com/muzafferus/borutoapp/domain/user_cases/GetSelectedHeroUseCase.kt
@@ -1,11 +1,12 @@
 package com.muzafferus.borutoapp.domain.user_cases
 
 import com.muzafferus.borutoapp.data.repository.Repository
+import com.muzafferus.borutoapp.domain.model.Hero
 
 class GetSelectedHeroUseCase(
-    private val repository:Repository
+    private val repository: Repository
 ) {
-    suspend operator fun invoke(heroId:Int){
-        repository.getSelectedHero(heroId = heroId)
+    suspend operator fun invoke(heroId: Int): Hero {
+        return repository.getSelectedHero(heroId = heroId)
     }
 }
---
+++ app/src/main/java/com/muzafferus/borutoapp/presentation/screens/details/DetailsViewModel.kt
@@ -0,0 +1,32 @@
+package com.muzafferus.borutoapp.presentation.screens.details
+
+import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.muzafferus.borutoapp.domain.model.Hero
+import com.muzafferus.borutoapp.domain.user_cases.UseCases
+import com.muzafferus.borutoapp.util.Constants.DETAILS_ARGUMENT_KEY
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailsViewModel @Inject constructor(
+    private val useCases: UseCases, savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private val _selectedHero: MutableState<Hero?> = mutableStateOf(null)
+    val selectedHero: State<Hero?> = _selectedHero
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            val heroId = savedStateHandle.get<Int>(DETAILS_ARGUMENT_KEY)
+            _selectedHero.value = heroId?.let { useCases.getSelectedHeroUseCase(heroId = heroId) }
+        }
+    }
+}
