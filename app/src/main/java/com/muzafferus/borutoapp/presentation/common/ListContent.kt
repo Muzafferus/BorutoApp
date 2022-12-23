dd4e0e5c19d30d288aaeaf72ba9619dd70419f8c
muzeffer.pasazade <muzeffer.pasazade@1tech.co>
2022-12-23 08:10:17 +0400

    Dependency Update, Code Cleanup

--- app/src/main/java/com/muzafferus/borutoapp/presentation/common/ListContent.kt
+++ app/src/main/java/com/muzafferus/borutoapp/presentation/common/ListContent.kt
@@ -1,7 +1,6 @@
 package com.muzafferus.borutoapp.presentation.common
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -26,7 +26,8 @@ import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.items
 import coil.annotation.ExperimentalCoilApi
-import coil.compose.rememberImagePainter
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.muzafferus.borutoapp.R
 import com.muzafferus.borutoapp.domain.model.Hero
 import com.muzafferus.borutoapp.navigation.Screen
@@ -95,10 +96,7 @@ fun HeroItem(
     hero: Hero,
     navController: NavHostController
 ) {
-    val painter = rememberImagePainter(data = "$BASE_URL${hero.image}") {
-        placeholder(R.drawable.ic_placeholder)
-        error(R.drawable.ic_placeholder)
-    }
+
 
     Box(
         modifier = Modifier
@@ -109,10 +107,14 @@ fun HeroItem(
         contentAlignment = Alignment.BottomStart
     ) {
         Surface(shape = RoundedCornerShape(size = LARGE_PADDING)) {
-            Image(
+            AsyncImage(
                 modifier = Modifier.fillMaxSize(),
-                painter = painter,
-                contentDescription = stringResource(R.string.hero_image),
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(data = "$BASE_URL${hero.image}")
+                    .placeholder(drawableResId = R.drawable.ic_placeholder)
+                    .error(drawableResId = R.drawable.ic_placeholder)
+                    .build(),
+                contentDescription = stringResource(id = R.string.hero_image),
                 contentScale = ContentScale.Crop
             )
         }
